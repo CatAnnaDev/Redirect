200f35c6b7a03d4f97ad0cc4f7946d86b069bc12
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-03-31 17:35:51 +0200

    Trick the Trickster

--- index.js
+++ index.js
@@ -1,119 +1,124 @@
-const Vec3 = require('tera-vec3');
-const config = require("./config.json");
+/** @format */
+
+const Vec3 = require("tera-vec3")
+const config = require("./config.json")
 
 module.exports = function Redirect(mod) {
-
- const blacklist = [9713];
- const whitelist = [9031, 9032, 3016];
- const command = mod.command || mod.require.command;
+  const blacklist = [9713]
+  const whitelist = [9031, 9032, 3016]
+  const command = mod.command || mod.require.command
 
   let enabled = config.enabled,
-      loot,
-      zone,
-      RKH,
-      RKN,
-      vsn,
-      hw,
-      opi,
-      Velika,
-      bahaar,
-      bahin,
-      catanm,
-      catahm,
-      banyaka = 81301,
-	    reset = false;
+    loot,
+    zone,
+    RKH,
+    RKN,
+    vsn,
+    hw,
+    opi,
+    Velika,
+    bahaar,
+    bahin,
+    Pla,
+    catahm,
+    banyaka = 81301,
+    reset = false
 
-  const hwredeem = new Vec3(22205, 4870, 6191);  //base pos H tp
-  const hwtp = new Vec3(21222, 5919, 6216);  //base pos book tp
-  const bahaarH = new Vec3(115023, 90044, 6377); //base pos H tp
-  const rkskip = new Vec3(-43963, 48750, 1); // Skip 1st boss RK9 HM / Rampage RK9 HM
-  const velik = new Vec3(1604, 3044, 1744)//Velika base tp 
-  const ba = new Vec3(-103510, 98460, 3547);
-  const opih = new Vec3(49500, 129289, 3720); // opi HM
-  const RKNM = new Vec3(-43486, 40629, -953);
-  const vsnm = new Vec3(43948, -134721, 29070);
-  const vslast = new Vec3(39581, -112922, 17213);
-  const chestloc = new Vec3(52562, 117921, 4431);
-  const cataNM = new Vec3(-79109, 119405, -1989);
-  const cataHM = new Vec3(-79109, 119405, -1989);
-  const chests = [81341, 81342];
+  const hwredeem = new Vec3(22205, 4870, 6191) //base pos H tp
+  const hwtp = new Vec3(21222, 5919, 6216) //base pos book tp
+  const bahaarH = new Vec3(115023, 90044, 6377) //base pos H tp
+  const rkskip = new Vec3(-43963, 48750, 1) // Skip 1st boss RK9 HM / Rampage RK9 HM
+  const velik = new Vec3(1604, 3044, 1744) //Velika base tp
+  const ba = new Vec3(-103510, 98460, 3547)
+  const opih = new Vec3(49500, 129289, 3720) // opi HM
+  const RKNM = new Vec3(-43486, 40629, -953)
+  const vsnm = new Vec3(43948, -134721, 29070)
+  const vslast = new Vec3(39581, -112922, 17213)
+  const chestloc = new Vec3(52562, 117921, 4431)
+  const PlaN = new Vec3(-131801, -52327, 5512)
+  const cataHM = new Vec3(-79109, 119405, -1989)
+  const chests = [81341, 81342]
 
   // open world
-  mod.hook('S_SPAWN_ME', 3, event => {
-    if (!enabled) return;
-    if (hw && hwredeem.dist3D(event.loc) <= 5 || hwtp.dist3D(event.loc) <= 5){
-    event.loc = new Vec3(19297, 4132, 6191) //banker
-    return true
+  mod.hook("S_SPAWN_ME", 3, (event) => {
+    if (!enabled) return
+    if ((hw && hwredeem.dist3D(event.loc) <= 5) || hwtp.dist3D(event.loc) <= 5) {
+      event.loc = new Vec3(19297, 4132, 6191) //banker
+      return true
     }
-    if (bahaar && bahaarH.dist3D(event.loc) <= 5){
-    event.loc = new Vec3(115321, 96917, 7196) // Bahaar portal
-    return true
+    if (bahaar && bahaarH.dist3D(event.loc) <= 5) {
+      event.loc = new Vec3(115321, 96917, 7196) // Bahaar portal
+      return true
     }
-    if (RKH && RKNM.dist3D(event.loc) <= 5){
-    event.loc = new Vec3(-41392, 40629, -953) //TP enter
-    return true	    
+    if (RKH && RKNM.dist3D(event.loc) <= 5) {
+      event.loc = new Vec3(-41392, 40629, -953) //TP enter
+      return true
     }
-    if (RKN && RKNM.dist3D(event.loc) <= 5){
-    event.loc = new Vec3(-41392, 40629, -953) //TP enter
-    return true	    
+    if (RKN && RKNM.dist3D(event.loc) <= 5) {
+      event.loc = new Vec3(-41392, 40629, -953) //TP enter
+      return true
     }
-    if (vsn && vsnm.dist3D(event.loc) <= 5){
-    event.loc = new Vec3(44353, -126459, 16788) //TP enter
-    return true	    
+    if (vsn && vsnm.dist3D(event.loc) <= 5) {
+      event.loc = new Vec3(44353, -126459, 16788) //TP enter
+      return true
     }
-    if (bahin && ba.dist3D(event.loc) <= 5){
-    event.loc = new Vec3(-98222, 99611, 4360)
-    return true
+    if (bahin && ba.dist3D(event.loc) <= 5) {
+      event.loc = new Vec3(-98222, 99611, 4360)
+      return true
     }
-    if (Velika && velik.dist3D(event.loc) <= 5){
-    event.loc = new Vec3(-607, 8777, 2173)
-    return true
+    if (Velika && velik.dist3D(event.loc) <= 5) {
+      event.loc = new Vec3(-607, 8777, 2173)
+      return true
     }
-    if (opi && opih.dist3D(event.loc) <= 5){
+    if (opi && opih.dist3D(event.loc) <= 5) {
       event.loc = new Vec3(4992, 128393, 3714)
       return true
-      }
-  });
-// dungeon
-  mod.hook('S_SPAWN_ME', 3, event => {
-    if (!enabled) return;
-    switch(zone) {
-        case 9713: // Ghillie
-            event.loc = new Vec3(52233, 117319, 4382)
-            event.w = 1.5
-            return true;
-        case 9031: // Ace Akasha	
-            event.loc = new Vec3(72424, 133968, -502)
-            event.w = 1.5
-            return true;
-        case 9032: // Ace Baracos
-            event.loc = new Vec3(28214, 178550, -1675)
-            event.w = 1.5
-            return true;
-        case 3016: // Ace Lilith	
-            event.loc = new Vec3(-99600, 58666, 8023)
-            event.w = 1.55
-            return true;
-        case 3027: // FA SLAYER	
-            event.loc = new Vec3(52261, 69448, -11743) 
-            event.w = 1.55
-            return true;
-        case 3103: // FAA BRAWLER	
-            event.loc = new Vec3(52276, 84804, -11743) 
-            event.w = 1.55
-            return true;
-        case 3203: // FAA BRAWLER HM
-            event.loc = new Vec3(52276, 84804, -11743) 
-            event.w = 1.55
-            return true;  
-        case 3102: // DRAAKON ARENA 	
-            event.loc = new Vec3(-115911, 130701, 20464) 
-            event.w = 1.55
-            return true;  
-        case 3202: // DRAAKON ARENA HM	
-            event.loc = new Vec3(-115911, 130701, 20464) 
-            event.w = 1.55
-            return true; 
+    }
+    if (Pla && PlaN.dist3D(event.loc) <= 5) {
+      event.loc = new Vec3(-130560, -53247, 4434)
+      return true
+    }
+  })
+  // dungeon
+  mod.hook("S_SPAWN_ME", 3, (event) => {
+    if (!enabled) return
+    switch (zone) {
+      case 9713: // Ghillie
+        event.loc = new Vec3(52233, 117319, 4382)
+        event.w = 1.5
+        return true
+      case 9031: // Ace Akasha
+        event.loc = new Vec3(72424, 133968, -502)
+        event.w = 1.5
+        return true
+      case 9032: // Ace Baracos
+        event.loc = new Vec3(28214, 178550, -1675)
+        event.w = 1.5
+        return true
+      case 3016: // Ace Lilith
+        event.loc = new Vec3(-99600, 58666, 8023)
+        event.w = 1.55
+        return true
+      case 3027: // FA SLAYER
+        event.loc = new Vec3(52261, 69448, -11743)
+        event.w = 1.55
+        return true
+      case 3103: // FAA BRAWLER
+        event.loc = new Vec3(52276, 84804, -11743)
+        event.w = 1.55
+        return true
+      case 3203: // FAA BRAWLER HM
+        event.loc = new Vec3(52276, 84804, -11743)
+        event.w = 1.55
+        return true
+      case 3102: // DRAAKON ARENA
+        event.loc = new Vec3(-115911, 130701, 20464)
+        event.w = 1.55
+        return true
+      case 3202: // DRAAKON ARENA HM
+        event.loc = new Vec3(-115911, 130701, 20464)
+        event.w = 1.55
+        return true
       /*  case 3104: // Catalepticon NM
             event.loc = new Vec3(-76328, 120434, -1684) 
             event.w = 1.55
@@ -121,120 +126,125 @@ module.exports = function Redirect(mod) {
         case 3204: // Catalepticon HM
             event.loc = new Vec3(-76328, 120434, -1684) 
             event.w = 1.55
-            return true;   */           			
-        default: return;
+            return true;   */
+      default:
+        return
     }
-});
+  })
 
+  mod.hook("S_SPAWN_NPC", 12, (event) => {
+    if (!enabled) return
+    if (event.huntingZoneId == 713 && chests.includes(event.templateId)) {
+      reset = true
+      command.message("Ghillieglade will be reset the next time you enter veliks sanctuary.")
+    }
+  })
 
-mod.hook('S_SPAWN_NPC', 12, event => {
-  if (!enabled) return;
-  if (event.huntingZoneId == 713 && chests.includes(event.templateId)) {
-    reset = true;
-    command.message('Ghillieglade will be reset the next time you enter veliks sanctuary.');
+  mod.hook("C_RESET_ALL_DUNGEON", 1, (event) => {
+    if (!enabled) return
+    if (mod.game.me.zone == 9713) {
+      reset = false
+      command.message("Ghillieglade was reset manually.")
+    }
+  })
+
+  mod.hook("S_SPAWN_DROPITEM", 9, (event) => {
+    if (!(blacklist.indexOf(event.item) > -1)) loot[event.gameId.toString()] = 1
+  })
+
+  mod.hook("S_DESPAWN_DROPITEM", 4, (event) => {
+    if (event.gameId.toString() in loot) delete loot[event.gameId.toString()]
+    if (Object.keys(loot).length < 1 && zone > 9000) resetinstance()
+  })
+
+  function resetinstance() {
+    if (!enabled) return
+    if ((zone == 9031 || zone == 9032 || zone == 3016) && whitelist.indexOf(zone) > -1) mod.send("C_RESET_ALL_DUNGEON", 1, null)
   }
-});
 
-mod.hook('C_RESET_ALL_DUNGEON', 1, event => {
-  if (!enabled) return;
-  if (mod.game.me.zone == 9713) {
-    reset = false;
-   command.message('Ghillieglade was reset manually.');
-  }
-});
+  mod.hook("S_BOSS_GAGE_INFO", 3, (event) => {
+    if (!enabled) return
+    if (((Number.parseInt(event.curHp) / Number.parseInt(event.maxHp)) * 10000) / 100 <= 20 && event.templateId == banyaka) {
+      teleport()
+    }
+  })
 
-mod.hook('S_SPAWN_DROPITEM', 9, event => {
-  if(!(blacklist.indexOf(event.item) > -1)) loot[event.gameId.toString()] = 1;
-});
-
-mod.hook('S_DESPAWN_DROPITEM', 4, event => {
-  if(event.gameId.toString() in loot) delete loot[event.gameId.toString()];
-  if(Object.keys(loot).length < 1 && zone > 9000) resetinstance();
-});
-
-function resetinstance() {
-  if (!enabled) return;
-  if((zone == 9031 || zone == 9032 || zone == 3016) && whitelist.indexOf(zone) > -1)  mod.send('C_RESET_ALL_DUNGEON', 1, null);
-}
-
-mod.hook('S_BOSS_GAGE_INFO',3,(event) => {
-  if(!enabled) return;
-    if ((Number.parseInt(event.curHp) / Number.parseInt(event.maxHp)*10000)/100 <= 20 && event.templateId == banyaka) {
-        teleport();
-  }
-  });	
-
-  mod.game.me.on('change_zone', (zone) => {
-		if (!enabled) return;
-		if (zone == 9714 && reset) {
-			mod.send('C_RESET_ALL_DUNGEON', 1, {});
-			reset = false;
-			mod.command.message('Ghillieglade has been reset.');
-		}
-  });
+  mod.game.me.on("change_zone", (zone) => {
+    if (!enabled) return
+    if (zone == 9714 && reset) {
+      mod.send("C_RESET_ALL_DUNGEON", 1, {})
+      reset = false
+      mod.command.message("Ghillieglade has been reset.")
+    }
+  })
 
   function teleport() {
-		mod.send('S_INSTANT_MOVE', 3, {
-				gameId: mod.game.me.gameId,
-				loc: chestloc,
-				w: 0.18
-			});
-		return false;
-	}
+    mod.send("S_INSTANT_MOVE", 3, {
+      gameId: mod.game.me.gameId,
+      loc: chestloc,
+      w: 0.18,
+    })
+    return false
+  }
 
   function pylon() {
-    if (RKH){
-		mod.toClient('S_INSTANT_MOVE', 3, {
-			gameId: mod.game.me.gameId,
-			loc: rkskip,
-			w: 3.12
-    });
-    sendMessage('Skip RK9 HM 1st Boss');
-    sendMessage('back normally and click with your mouse to tp at the 2nd boss');
-  } else { sendMessage('Not in RH9 HM')}
+    if (RKH) {
+      mod.toClient("S_INSTANT_MOVE", 3, {
+        gameId: mod.game.me.gameId,
+        loc: rkskip,
+        w: 3.12,
+      })
+      sendMessage("Skip RK9 HM 1st Boss")
+      sendMessage("back normally and click with your mouse to tp at the 2nd boss")
+    } else {
+      sendMessage("Not in RH9 HM")
+    }
   }
-  
+
   function last() {
-    if (vsn){
-		mod.toClient('S_INSTANT_MOVE', 3, {
-			gameId: mod.game.me.gameId,
-			loc: vslast,
-			w: 3.12
-    });
-    sendMessage('Last Boss Veliks Sanctuary');
-  } else { sendMessage('Not in Veliks Sanctuary')}
+    if (vsn) {
+      mod.toClient("S_INSTANT_MOVE", 3, {
+        gameId: mod.game.me.gameId,
+        loc: vslast,
+        w: 3.12,
+      })
+      sendMessage("Last Boss Veliks Sanctuary")
+    } else {
+      sendMessage("Not in Veliks Sanctuary")
+    }
   }
-  
 
-  mod.hook('S_LOAD_TOPO', 3, event => {
-    vsn = (event.zone === 9781)
-    bahaar = (event.zone === 7004)
-    hw = (event.zone === 7031)
-    bahin = (event.zone === 9004)
-    RKN = (event.zone === 9735)
-    RKH = (event.zone === 3034)
-    Velika = (event.zone === 7005)
-    opi = (event.zone === 3036)
-    zone = event.zone;
-    loot = {};
-});
+  mod.hook("S_LOAD_TOPO", 3, (event) => {
+    vsn = event.zone === 9781
+    bahaar = event.zone === 7004
+    hw = event.zone === 7031
+    bahin = event.zone === 9004
+    RKN = event.zone === 9735
+    RKH = event.zone === 3034
+    Velika = event.zone === 7005
+    opi = event.zone === 3036
+    zone = event.zone
+    loot = {}
+  })
 
-    function sendMessage(msg) { mod.command.message(msg) }
+  function sendMessage(msg) {
+    mod.command.message(msg)
+  }
 
-  command.add('redirect', {
-    '$default': () => {
-      enabled = !enabled;
-    sendMessage(enabled ? 'Redirect enabled.' : 'Redirect disabled.');
-  },    
-  'rk': () => {
-    pylon();
-},
-'vs': () => {
-  last();
+  command.add("redirect", {
+    $default: () => {
+      enabled = !enabled
+      sendMessage(enabled ? "Redirect enabled." : "Redirect disabled.")
+    },
+    rk: () => {
+      pylon()
+    },
+    vs: () => {
+      last()
+    },
+  })
+
+  this.destructor = function () {
+    command.remove("redirect")
+  }
 }
-});
-  
-  this.destructor = function() {
-    command.remove('redirect');
-  };
-};
